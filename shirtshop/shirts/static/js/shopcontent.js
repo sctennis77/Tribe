2df729798ffa63c075abf0f9e42029083246660a
Sam <sam@eproject-inc.com>
2012-12-03 23:27:28 -0500

    updated git to current

--- shirtshop/shirts/static/js/shopcontent.js
+++ shirtshop/shirts/static/js/shopcontent.js
@@ -11,52 +11,28 @@
 		$.post(url,shirt_data, {} ,'json')
 	});
 
-	// modified from bxSlider example
-	$(document).ready(function(){
-    	$('.bx_slider').bxSlider({
-    		controls:false,
-    		prevText:"",
-    		nextText:""
-    	});
-  	});
-
-	$(function(){
-  // assign the slider to a variable
- 		 var slider = $('.bx_slider').bxSlider({
-    	controls: false,
-  	});
-
- 
-
-
 
    // assign a click event to the external thumbnails
     $('.thumbs a').click(function(){
       // jequery element to change description and pattern name
     
+      console.log("yo");
       // jquery for thumbnail click
       var thumbIndex = $('.thumbs a').index(this);
       change_shirt_info(thumbIndex);
+      var pic = $('.main_picture');
+      console.log(pic);
+    });
 
       // change description, pattern based on shirt clicked
    
 
 
 
-      slider.goToSlide(thumbIndex);
-
-  
-    // remove all active classes
-    $('.thumbs a').removeClass('pager-active');
-    // assisgn "pager-active" to clicked thumb
-    $(this).addClass('pager-active');
-    // very important! you must kill the links default behavior
-      return false;
-    });
+      
 
     function change_shirt_info(idx)
     {
-      console.log(idx);
       var description_text_html = $('.shirt_description .description_text');
       var pattern_name_html = $('.shirt_description .pattern_name');
       // instead of shirt_list[0] this will be shirt_list[idx] where idx
@@ -73,6 +49,5 @@
     }
 
   // assign "pager-active" class to the first thumb
- 	$('.thumbs a:first').addClass('pager-active');
-	});
+ 
 
